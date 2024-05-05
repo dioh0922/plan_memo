9bf8987b673742f68469639be8bf2554ca941708
dioh0922 <diohound@gmail.com>
2024-05-05 10:37:57 +0900

    ヘッダーに変更

--- app.vue
+++ app.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <h1>実現機能ネタまとめ</h1>
-    <RouteList @open="openIdea"/>
-    <DetailDialog 
-    @close="closeDialog" 
-    :active="dialog.disp"
-    :summary="dialog.summary"
-    :detail="dialog.detail"/>
+    <v-app>
+      <v-main>
+        <v-app-bar color="primary">
+          <v-app-bar-title>
+            実現機能ネタまとめ
+          </v-app-bar-title>
+          <template v-slot:append>
+            <RegisterDialog/>
+          </template>
+        </v-app-bar>
+        <RouteList @open="openIdea"/>
+        <DetailDialog 
+        @close="closeDialog" 
+        :active="dialog.disp"
+        :summary="dialog.summary"
+        :detail="dialog.detail"/>
+      </v-main>
+    </v-app>
   </div>
 </template>
 
