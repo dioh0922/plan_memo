a1ee2b79c1f2466caa089e4420f86b9c5d748a88
dioh0922 <diohound@gmail.com>
2024-05-08 09:52:03 +0900

    一覧で削除フラグを判定するように追加

--- server/api/list.ts
+++ server/api/list.ts
@@ -1,5 +1,9 @@
 export default defineEventHandler(async (event) => {
-    const list = await prisma.plan_memo.findMany()
+    const list = await prisma.plan_memo.findMany({
+      where: {
+        is_delete: false
+      }
+    })
     return {
       list: list,
     }
